349c082ba2cd563f5198de64d6fc3b7ba4873a13
viniarnhold <viniciusarnh@gmail.com>
2021-08-20 09:58:48 -0300

    Explorando novas IDE's(Eclipse e IntelliJ)

---
+++ modulo2/aula6/src/AlunoComArgumento.java
@@ -0,0 +1,10 @@
+public class AlunoComArgumento {
+
+    public static void main(String[] args){
+
+        String aluno = "Vinícius";
+
+        System.out.println("Eu sou aluno do Level 1 do CWI Reset! Me chamo " + args[0]);
+
+    }
+}
