1f55f553e905e0e18f2f63feb0778f53125d415a
viniarnhold <viniciusarnh@gmail.com>
2021-09-01 16:28:17 -0300

    Novos métodos para SMP - Implementação de moedas e vidas

--- modulo3/supermarioprogram/src/Main.java
+++ modulo3/supermarioprogram/src/Main.java
@@ -2,22 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Mario mario = new Mario();
 
-        Mario mario2 = new Mario("Mario2", 45, 1.50);
-
-        System.out.println("");
-        System.out.println("Construtor 1");
         mario.caracteristicas();
 
-        System.out.println("");
-        System.out.println("Construtor 2");
-        mario2.caracteristicas();
+        mario.pegarMoeda();
 
-        mario2.superHumanStrength();
-        System.out.println("Status após Mario2 utilizar sua Super Força");
-        mario2.caracteristicas();
+        mario.morrer();
+        mario.morrer();
 
-        mario.crescer();
-        System.out.println("Status após Mario utilizar o crescer");
         mario.caracteristicas();
 
     }
--- modulo3/supermarioprogram/src/Mario.java
+++ modulo3/supermarioprogram/src/Mario.java
@@ -2,17 +2,19 @@ public class Mario {
     private String nome;
     private Integer idade;
     private Double altura;
-    private String personalidade;
     private Integer forca;
     private Integer estamina;
+    private Integer moedasColetadas;
+    private Integer vidas;
 
     public Mario() {
         this.nome = "Mario Bross";
         this.idade = 40;
         this.altura = 1.50;
         this.estamina = 100;
-        this.personalidade = "Empolgado";
         this.forca = 80;
+        this.moedasColetadas = 0;
+        this.vidas = 1;
     }
 
     public Mario(String nome, Integer idade, Double altura) {
@@ -20,56 +22,9 @@ public class Mario {
         this.idade = idade;
         this.altura = altura;
         this.estamina = 100;
-        this.personalidade = "Empolgado";
         this.forca = 80;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-
-    public Double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(Double altura) {
-        this.altura = altura;
-    }
-
-    public String getPersonalidade() {
-        return personalidade;
-    }
-
-    public void setPersonalidade(String personalidade) {
-        this.personalidade = personalidade;
-    }
-
-    public Integer getForca() {
-        return forca;
-    }
-
-    public void setForca(Integer forca) {
-        this.forca = forca;
-    }
-
-    public Integer getEstamina() {
-        return estamina;
-    }
-
-    public void setEstamina(Integer estamina) {
-        this.estamina = estamina;
+        this.moedasColetadas = 0;
+        this.vidas = 1;
     }
 
     public void perderEstamina(){
@@ -86,21 +41,44 @@ public class Mario {
         }
     }
 
+    public void revigorar(){
+        this.estamina = 100;
+    }
+
+    public void morrer(){
+        this.estamina = 0;
+        this.vidas -= 1;
+        if(this.vidas < 0){
+            this.vidas = 0;
+        } else {
+            revigorar();
+        }
+    }
+
+    public void pegarMoeda(){
+        this.moedasColetadas += 1;
+        if(this.moedasColetadas >= 10){
+            this.vidas += 1;
+            this.moedasColetadas = 0;
+        }
+    }
+
     public void crescer(){
         this.altura *= 2;
     }
 
     public void caracteristicas(){
+        System.out.println("-------CARACTERÍSTICAS-------");
         System.out.println("Nome: " + this.nome);
         System.out.println("Idade: " + this.idade);
         System.out.printf("Altura: %.2f\n", this.altura);
-        System.out.println("Personalidade: " + this.personalidade);
         System.out.println("Força: " + this.forca);
         System.out.println("Estamina: " + this.estamina);
+        System.out.println("Moedas coletadas: " + this.moedasColetadas);
+        System.out.println("Vidas: " + this.vidas);
     }
     public void superHumanStrength(){
         if (this.forca != null) {
-            this.personalidade = "Confiante e irritado";
             this.forca *= 1000;
         }
         else {
